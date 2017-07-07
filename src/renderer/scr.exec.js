3bc00f0fea5dc4dfe254c44d30333998a80fc0ab
dorgon <dorgonman@hotmail.com>
2017-07-07 21:26:18 +0800

    up

--- src/renderer/scr.exec.js
+++ src/renderer/scr.exec.js
@@ -39,4 +39,39 @@ document.addEventListener("DOMContentLoaded", () => {
     GUI.updateMessageList(messages);
     GUI.scrollMessagesToTop();
   });
+
+
+  //var xmlhttp = new XMLHttpRequest();
+  //var url = 'http://horizon-studio.net/ue4/discord_history_tracker/dht.txt?name=1';
+  var url = 'file:///C:/workspace/Discord-History-Tracker/bld/dht.txt'
+  //var url = 'http://mathiasbynens.be/demo/ip';
+  //var url = 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd';
+
+
+  //var url = 'http://horizon-studio.net/ue4/discord_history_tracker/dht.json';
+  //http://horizon-studio.net/ue4/discord_history_tracker/dht.json
+  var xhr = new XMLHttpRequest();
+  //xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.setRequestHeader("Access-Control-Allow-Origin","*")
+  xhr.onreadystatechange = processRequest;
+  xhr.open('GET', url);
+
+  //xhr.setRequestHeader('Content-Type', 'text/plain');
+  xhr.send(null);
+
+ // xhr.addEventListener("readystatechange", processRequest, false);
+
+  function processRequest(e) {
+    alert('polling');
+    if (xhr.readyState == 4) {
+        // time to partay!!!
+         if (xhr.status === 200 || xhr.status === 0) {
+            alert("success:" + xhr.responseText);
+          }
+          
+    }
+  }
+
+
+
 });
